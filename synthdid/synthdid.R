b8e62cbf0b23d8520669dcbfe5130741c86b5a61
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-05-14 08:52:03 +0700

    Use synthdid With (In) R Software

--- synthdid/synthdid.R
+++ synthdid/synthdid.R
@@ -11,7 +11,7 @@ setup = panel.matrices(synthdid)
 synthdid = synthdid_estimate(setup$Y, setup$N0, setup$T0)
 synthdid
 summary(synthdid)
-Synthetic Difference In Differences (DID) Estimation Use synthdid With (In) R Software
+# Synthetic Difference In Differences (DID) Estimation Use synthdid With (In) R Software
 # Olah Data Semarang
 # WhatsApp : +6285227746673
 # IG : @olahdatasemarang_
