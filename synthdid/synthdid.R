b15dcac04cacf6c4e89fe8910267f68cfc394c46
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-05-14 08:44:10 +0700

    Use synthdid With (In) R Software

---
+++ synthdid/synthdid.R
@@ -0,0 +1,19 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Synthetic Difference In Differences (DID) Estimation Use synthdid With (In) R Software
+install.packages("remotes")
+remotes::install_github("synth-inference/synthdid")
+library("synthdid")
+spml_one = read.csv("https://raw.githubusercontent.com/timbulwidodostp/synthdid/main/synthdid/synthdid.csv",sep = ";")
+# Estimate Synthetic Difference In Differences (DID) Estimation Use synthdid With (In) R Software
+setup = panel.matrices(synthdid)
+synthdid = synthdid_estimate(setup$Y, setup$N0, setup$T0)
+synthdid
+summary(synthdid)
+Synthetic Difference In Differences (DID) Estimation Use synthdid With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
+
